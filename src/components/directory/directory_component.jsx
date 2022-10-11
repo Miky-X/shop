a8be901a5b0c5e91d7de9e691d9e2574814497fd
Michał Kaczmarek <michal@MacBook-Pro-Micha.local>
2022-10-11 18:51:55 +0200

    updating pur directory page with directory component and category item component

---
+++ src/components/directory/directory_component.jsx
@@ -0,0 +1,14 @@
+import CategoryItem from "../categoryItem/categoryItem_commponent";
+import './directory_style.scss'
+
+const Directory = ({categories}) => {
+    return (
+        <div className='directory-container'>
+            {categories.map((category) => (
+                <CategoryItem key={category.id} category={category} />
+            ))}
+        </div>
+    );
+};
+
+export default Directory;
