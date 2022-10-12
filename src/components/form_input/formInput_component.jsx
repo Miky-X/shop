2ea9d23048012d3a4352c0e459b42f862c2817fc
Michał Kaczmarek <michal@MacBook-Pro-Micha.local>
2022-10-12 16:07:14 +0200

    Created sign up component, the form input component, botton component

---
+++ src/components/form_input/formInput_component.jsx
@@ -0,0 +1,14 @@
+import './formInput_styles.scss';
+
+const Forminput = ({label, ...otherProps}) => {
+    return (
+        <div className="group">
+            <input className="form-input" {...otherProps} />
+            {
+                label && (
+                    <label className={`${otherProps.value.length ? 'shrink' : ''} form-input-label`}>{label}</label>)}
+        </div>
+    )
+}
+
+export default Forminput
--- src/routes/sign_in/signIn_component.jsx
+++ src/routes/sign_in/signIn_component.jsx
@@ -1,14 +1,17 @@
-import { signInWithGooglePopup} from "../../utils/firebase/firebase_utils";
+import { signInWithGooglePopup, createUserDocumentFromAuth } from "../../utils/firebase/firebase_utils";
+import SignUpForm from "../../components/sign_up/signUpForm_component";
 
 const SignIn = () => {
     const logGoogleUser = async () => {
-        const responce = await signInWithGooglePopup();
-        console.log(responce);
-    }
+        const { user } = await signInWithGooglePopup();
+        const userDocRef = await createUserDocumentFromAuth(user);
+    };
+
     return (
         <div>
             <h1>Sign In Page</h1>
             <button onClick={logGoogleUser}>Sign in with Google Popup</button>
+            <SignUpForm />
         </div>
     )
 };
